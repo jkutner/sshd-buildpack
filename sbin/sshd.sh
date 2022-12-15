29815f4b691adf922703714bcfb8b15d55f046f6
Ben Chiciudean <benydc@yahoo.com>
2022-12-15 12:56:51 +0200

    feat: add sshd path optionally via env var

--- sbin/sshd.sh
+++ sbin/sshd.sh
@@ -14,7 +14,12 @@ EOF
 
   chmod 600 /workspace/.ssh/*
 
+  sshd_path="/usr/sbin/sshd"
+  if [[ -n "${SSHD_PATH}" ]]; then
+    sshd_path=$SSHD_PATH
+  fi
+
   ssh_port=${SSH_PORT:-"2222"}
   echo "at=sshd state=starting user=$(whoami) port=${ssh_port}"
-  /usr/sbin/sshd -f $ssh_dir/sshd_config -o "Port ${ssh_port}"
+  $sshd_path -f $ssh_dir/sshd_config -o "Port ${ssh_port}"
 fi
